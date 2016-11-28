46b2e3e690598fb3170528a68bb83cf25253e585
Chauhan <rchauhan@ptc.com>
2016-11-28 10:48:26 +0530

    changes

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-(function(){
-  'use strict';
-  angular.module('myFirstApp',[])
-  .controller('MyFirstController',function($scope){
-    $scope.name="Ruchika";
+(function()
+{
 
+  'use strict';
+    angular.module('NameCalculator',[])
+    .controller('NameCalculatorController',function($scope)
+     {
+      $scope.name="";
+      $scope.totalValue=0;
+      //console.log($scope);
+
+      $scope.displayNumeric = function()
+       {
+        var totalNameValue=calculateNumericForString($scope.name);
+        $scope.totalValue = totalNameValue;
+      //  console.log($scope);
+       };
+
+
+      function calculateNumericForString(string)
+      {
+        var totalStringValue=0;
+        for(var i=0;i<string.length;i++)
+        {
+          totalStringValue += string.charCodeAt(i);
+        }
+
+
+        return totalStringValue;
+
+
+      };
   });
 
 })();
