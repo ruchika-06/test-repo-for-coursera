485d9b36222d4cfd420530c6ae2258590970626b
Chauhan <rchauhan@ptc.com>
2016-11-29 11:11:36 +0530

    new

--- DIapp.js
+++ DIapp.js
@@ -1,11 +1,27 @@
 (function(){
   'use strict';
   angular.module('DIApp',[]).controller('DIController',DIController);
-  function DIController($scope,$filter){
+  DIController.$inject =['$scope','$filter'];
+
+function DIController($scope,$filter){
     $scope.name="Yakoov";
-    $scope.upper=function(){
-      var upCase=$filter('uppercase');
-      $scope.name=upCase($scope.name);
-    };
-  }
+    $scope.state="hungry";
+    $scope.sayMessage=function()
+    {
+      return"Yakoov likes to eat";
+    }
+    // $scope.upper=function(){
+    //   var upCase=$filter('uppercase');
+    //   $scope.name=upCase($scope.name);
+    // };
+
+
+  $scope.feed = function()
+  {
+    $scope.state ="fed";
+  };
+
+}
 })();
+//minification
+//!function(){"use strict";function e(e,n){e.name="Yakoov",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
